bc9f38bb88e0d2afdc7a4199e92a15145e4d299b
YotaMarkman <yotam.markman@post.runi.ac.il>
2023-12-28 14:13:51 +0200

    HW2

--- Reverse.java
+++ Reverse.java
@@ -7,7 +7,7 @@ public class Reverse
 		for ( int i = word.length() - 1; i >= 0; i-- )
 		{
 			System.out.print ( word.charAt(i) );
-			if ( i == word.length()/2 )
+			if ( i == word.length()/2 - 1 )
 			{
 				middle = word.charAt(i);
 			}
